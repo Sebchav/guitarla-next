02197d8d5e740a96b9c64f3c5bb6911f892db28d
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-12 12:33:32 -0600

    Footer

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,29 @@
+import Link from "next/link"
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className="contenedor">
+            <nav className="navegacion">
+                    <Link href="/" >
+                        Inicio
+                    </Link>
+
+                    <Link href="/nosotros" >
+                        Nosotros
+                    </Link>
+
+                    <Link href="/blog"> 
+                        Blog
+                    </Link>
+
+                    <Link href="/tienda">
+                        Tienda
+                    </Link>
+                </nav>
+
+                <p>Todos los derechos reservados {new Date().getFullYear()}</p>
+        </div>
+    </footer>
   )
 }
