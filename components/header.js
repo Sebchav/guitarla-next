34cfacb72ad829ee91d38f7c23b307db1fe0ab20
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-12 12:02:55 -0600

    Header.css

--- components/header.js
+++ components/header.js
@@ -1,11 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import styles from "../styles/header.module.css"
 
 export default function Header() {
   return (
     <header className={styles.header}>
-        <div className="contenedor">
+        <div className={`contenedor ${styles.barra}`}>
             <Image src="/img/logo.svg" width={300} height={40} alt="Imagen logotipo"/>
 
             <nav className={styles.navegacion}>
