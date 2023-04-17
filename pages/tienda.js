808dc41af5f5850285aeed084b8344691f07645c
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-04-16 21:16:48 -0600

    CSS modules

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout"
 import Guitarra from "@/components/guitarra"
+import styles from "../styles/grid.module.css"
 
 export default function Tienda({guitarras}) {
 
@@ -12,16 +13,31 @@ export default function Tienda({guitarras}) {
       <main className="contenedor">
           <h1 className="heading">Nuestra Colección</h1>
 
+          <div className={styles.grid}>
+
           {guitarras?.map(guitarra=>(
             <Guitarra 
               key={guitarra.id}
               guitarra={guitarra.attributes}
             />
           ))}
+
+          </div>
       </main> 
     </Layout>
     
   )
+}
+
+export async function getServerSideProps(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+  const {data: guitarras} = await respuesta.json();
+  
+  return {
+    props: {
+      guitarras
+    } 
+  }
 }
 
 // export async function getStaticProps(){
@@ -34,14 +50,3 @@ export default function Tienda({guitarras}) {
 //     } 
 //   }
 // }
-
-export async function getServerSideProps(){
-  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
-  const {data: guitarras} = await respuesta.json();
-  
-  return {
-    props: {
-      guitarras
-    } 
-  }
-}
